c3a4054debc48b33591f0f5e47cc324998d76fe8
Nando <fernandofewp@hotmail.com>
2022-09-12 22:15:54 -0300

    fix target

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                 <h1>DSMeta</h1>
                 <p>
                     Desenvolvido por
-                    <a href="https://www.linkedin.com/in/fernando-souza-499579149/"> @Fernando Souza</a>
+                    <a target="_blank" href="https://www.linkedin.com/in/fernando-souza-499579149/"> @Fernando Souza</a>
                 </p>
             </div>
         </header>
